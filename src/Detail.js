cf3dd24a27f31ea4d6f39bf94101edeb2ce5cbc9
김 수정 <kim@gim-ui-iMac.local>
2022-05-24 23:39:39 +0900

    Add reactroute

---
+++ src/Detail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Detail = (props) => {
+  
+    return (
+      <h1>상세 페이지입니다!</h1>
+      
+    )
+
+}
+
+export default Detail;
